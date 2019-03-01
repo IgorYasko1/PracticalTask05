b3127ca4d6487013bd8e547efcf3d4fcd41a3524
IgorYasko1 <igoryasko121@yandex.ru>
2019-03-01 05:40:11 +0300

    add new method findRandomNumber

--- headsortails/HeadsOrTailsLogic.java
+++ headsortails/HeadsOrTailsLogic.java
@@ -9,10 +9,14 @@ public class HeadsOrTailsLogic {
     static int amountOfHeads;
     static int amountOfTails;
 
+    static int findRandomNumber(){
+        int random = new Random().nextInt(2);
+        return random;
+    }
+
     static void countHeadORTail(){
         for (int i = 0; i < FLIP; i++){
-            int random = new Random().nextInt(2);
-            if (random > 0){
+            if (findRandomNumber() > 0){
                 amountOfHeads++;
             }else{
                 amountOfTails++;
